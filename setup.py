03365ecb8fb0b09dc63dada9da3d1a6a8fb03165
gesellkammer <eduardo.moguillansky@gmail.com>
2023-02-18 17:58:39 +0100

    version

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ classifiers = """
 """
 
 setup(name='pitchtools',
-      version='1.9.0',
+      version='1.9.1',
       python_requires=">=3.9",
       description='Utilities to convert between midinotes, frequency and notenames', 
       long_description=open('README.rst').read(),
